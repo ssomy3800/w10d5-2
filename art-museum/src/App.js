a11245b44b117db12bd78f09ac34bb3763f969fd
ssomy3800 <ssomy3800@gmail.com>
2023-05-12 14:33:37 -0700

    commit

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -1,6 +1,11 @@
+import harvardArt from "./data/harvardArt.js";
+import { GalleryNavigation } from "./components/GalleryNavigation/index.js";
+
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    // <h1>Hello from App</h1>
+    console.log(harvardArt)
   );
 }
 
---
+++ art-museum/src/components/GalleryNavigation/index.js
@@ -0,0 +1,11 @@
+import harvardArt from ".../data/harvardArt.js";
+
+const galleries = (props) => {
+
+};
+
+const GalleryNavigation = () => {
+  return props;
+};
+
+export default GalleryNavigation;
